3f5fb204cf149c8a555254be741acb44dc5e5516
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-17 23:37:54 +0300

    feat: add path examples

---
+++ refs/os.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.homedir());
---
+++ refs/path.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+console.log(path.extname(__filename).slice(1));
+
+console.log(path.parse(__filename));
+console.log(path.resolve(__dirname, '..', './modules', './app.js'));
+console.log(path.join(__dirname, '..', './modules', './app.js'));
