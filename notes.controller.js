56971db32c7384ca2b9c814c00145cd9d721fc50
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-17 22:38:33 +0300

    feat: add read files

--- notes.controller.js
+++ notes.controller.js
@@ -1,8 +1,10 @@
 const fs = require('fs/promises');
+const path = require('path');
+
+const notesPath = path.join(__dirname, 'db.json');
 
 async function addNote(title) {
-  const notes = require('./db.json');
-
+  const notes = await getNotes();
   const note = {
     title,
     id: Date.now().toString(),
@@ -10,11 +12,13 @@ async function addNote(title) {
 
   notes.push(note);
 
-  await fs.writeFile('./db.json', JSON.stringify(notes));
+  await fs.writeFile(notesPath, JSON.stringify(notes));
 }
 
-function getNotes() {
-  return require('./db.json');
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: 'utf-8' });
+
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
 }
 
 module.exports = {
