6b28451cc421686978484ca4bc98df635068b47e
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-18 23:13:10 +0300

    feat: add ejs, show notes

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const http = require('http');
 const express = require('express');
 const chalk = require('chalk');
-const fs = require('fs/promises');
-const path = require('path');
 
-const { addNote } = require('./notes.controller');
+const { addNote, getNotes } = require('./notes.controller');
 
 const port = 3000;
-
-const basePath = path.join(__dirname, 'pages');
-
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'pages');
 
 app.use(
   express.urlencoded({
@@ -18,13 +15,19 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(basePath, 'index.html'));
+app.get('/', async (req, res) => {
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+  });
 });
 
 app.post('/', async (req, res) => {
   await addNote(req.body.title);
-  res.sendFile(path.join(basePath, 'index.html'));
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+  });
 });
 
 app.listen(port, () => {
--- notes.controller.js
+++ notes.controller.js
@@ -45,4 +45,5 @@ module.exports = {
   addNote,
   printNotes,
   removeNote,
+  getNotes,
 };
