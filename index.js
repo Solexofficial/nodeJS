47b964e03d84495fbf96bdd31c4e55e4703f88e3
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-18 22:49:19 +0300

    feat: add nodemon, listen post request

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const http = require('http');
 const chalk = require('chalk');
+const fs = require('fs/promises');
+const path = require('path');
+
+const { addNote } = require('./notes.controller');
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log('Server!');
+const basePath = path.join(__dirname, 'pages');
 
-  res.end('Hello from server');
+const server = http.createServer(async (req, res) => {
+  if (req.method === 'GET') {
+    const content = await fs.readFile(path.join(basePath, 'index.html'));
+    // res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    res.end(content);
+  } else if (req.method === 'POST') {
+    const body = [];
+    res.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8',
+    });
+
+    req.on('data', data => {
+      body.push(Buffer.from(data));
+    });
+
+    req.on('end', () => {
+      const title = body.toString().split('=')[1].replaceAll('+', ' ');
+      addNote(title);
+      res.end(`Title = ${title}`);
+    });
+  }
 });
 
 server.listen(port, () => {
