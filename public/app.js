333fc2c2d9154144d703f95cf2416ccb80c17be2
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-22 23:32:22 +0300

    feat: add note edit mode

--- public/app.js
+++ public/app.js
@@ -1,19 +1,63 @@
-document.addEventListener('click', e => {
-  if (e.target.dataset.type === 'edit') {
-    const id = e.target.dataset.id;
-    const title = e.target.closest('div').previousElementSibling.textContent;
-    const newTitle = prompt('Введите новое название', title);
-    if (newTitle === null) return;
+let editMode = false;
 
-    if (title !== newTitle) {
-      edit(id, newTitle).then(
-        data => (e.target.closest('div').previousElementSibling.textContent = JSON.parse(data).title)
-      );
+function toggleEditMode() {
+  return (editMode = !editMode);
+}
+
+function getNoteId(e) {
+  return e.target.closest('li').dataset.id;
+}
+
+document.addEventListener('click', e => {
+  function renderNote() {
+    const title = e.target.closest('div').previousElementSibling.textContent;
+    const noteParentNode = e.target.closest('li');
+
+    if (editMode) {
+      noteParentNode.innerHTML = `
+      <input type="text" class="note-title" value="${title}" />
+      <div class="btn-group">
+      <button class="btn btn-success" data-type="save">Сохранить</button>
+      <button class="btn btn-danger" data-type="cancel">Отменить</button>
+      </div>
+      `;
+    } else {
+      const title = e.target.closest('div').previousElementSibling.value;
+      noteParentNode.innerHTML = `
+        <span>${title}</span>
+                  <div class="btn-group">
+                    <button class="btn btn-primary" data-type="update">Обновить</button>
+                    <button class="btn btn-danger" data-type="remove">&times;</button>
+                  </div>
+        `;
     }
   }
+
+  if (e.target.dataset.type === 'update') {
+    toggleEditMode();
+    renderNote();
+  }
+
+  if (e.target.dataset.type === 'save') {
+    const id = getNoteId(e);
+    const title = e.target.closest('div').previousElementSibling.value;
+
+    update(id, title)
+      .then(data => (e.target.closest('div').previousElementSibling.textContent = JSON.parse(data).title))
+      .then(() => {
+        toggleEditMode();
+        renderNote();
+      });
+  }
+
+  if (e.target.dataset.type === 'cancel') {
+    toggleEditMode();
+    renderNote();
+  }
+
   if (e.target.dataset.type === 'remove') {
-    const id = e.target.dataset.id;
-    remove(id).then(() => {
+    const noteId = getNoteId(e);
+    remove(noteId).then(() => {
       e.target.closest('li').remove();
     });
   }
@@ -25,7 +69,7 @@ async function remove(id) {
   });
 }
 
-async function edit(id, title) {
+async function update(id, title) {
   const data = JSON.stringify({ id, title });
   await fetch(`/${id}`, {
     method: 'PUT',
